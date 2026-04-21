e33711d5c71864e33d4ff6157155e9669a2895dc
student <student@itismeucci.com>
2026-04-21 12:01:50 +0200

    correct map links

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 var map = L.map('map').setView([41.8902, 12.4922], 13);
 
-L.tileLayer('https://openstreetmap.org{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; OpenStreetMap'
+    attribution: '&copy; <a href="https://openstreetmap.org">OpenStreetMap</a>'
 }).addTo(map);
+
 
 aggiungiMonumenti(map);
 mostraStatisticheMonumenti();
-
-
